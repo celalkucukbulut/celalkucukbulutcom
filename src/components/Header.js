3d4cb7b430db65f3aa1e971d03955ab37dd06de5
Celal Küçükbulut <celalkucukbulut@gmail.com>
2021-07-29 17:28:31 +0300

    profil and header component css work

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 import { firebase } from '../firebase/firebase';
-
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
 
 
 export const Header = () => {
   var isAdmin = firebase.auth().currentUser != null;
 
   return (
-    <header className="headerPage">
-      <div className="content-container">
-        <div className="header__content">
-          {isAdmin && <button className="button button--link" onClick={startLogout}>Logout</button>}
+    <header className="header">
+      <div className="header__content">
+        <div className="header__home">
+          <IconButton color="inherit" aria-label="delete" size="medium"><HomeIcon fontSize="large"/></IconButton>
         </div>
+        <div className="header__options">
+          <button className="button__headerItem">RESUME</button>
+          <button className="button__headerItem">CONTACT</button>
+        </div>
+        {isAdmin && <button className="button button--link" onClick={startLogout}>Logout</button>}
       </div>
     </header>
   );
--- src/components/Profil.js
+++ src/components/Profil.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import photo from '../Images/profilPhoto.jpg';
+import Button from '@material-ui/core/Button';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 const Profil = () => {
 
@@ -10,9 +12,18 @@ const Profil = () => {
       <span className = "profil__job">Software Developer</span>
     </div>
     <img src={photo} alt="profilFoto" className="img" />
-    <p className="profil__info profil__listItem"><span className="profil__value">Birthday : </span>26.09.1993</p>
-    <p className="profil__info profil__listItem"><span className="profil__value">Location :</span> Kastamonu</p>
-    <p className="profil__info profil__listItem"><span className="profil__value">Email :</span> celalkucukbulut@gmail.com</p>
+    <div className="profil__info">
+      <p><span className="profil__value">Birthday : </span>26.09.1993</p>
+      <p><span className="profil__value">Location :</span> Kastamonu</p>
+      <p><span className="profil__value">Email :</span> celalkucukbulut@gmail.com</p>
+    </div>
+    <div className = "profil__cv">
+      <Button
+        variant="contained"
+        color="primary"
+        endIcon={<GetAppIcon></GetAppIcon>}
+      >Download Cv</Button>
+    </div>
   </div>
 )};
 
